37926c57e32c3cbaa1669d03bec157463ee0a06f
Vivekanand P V <vivekanandpv@outlook.com>
2020-12-12 14:35:14 +0530

    Add project files.

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DanskeIT.DotNetCore.Demo2.Service;
+
+namespace DanskeIT.DotNetCore.Demo2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        private readonly Book _book;
+
+        public SampleController(Book book)
+        {
+            _book = book;
+            Console.WriteLine("creating the controller");
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new {Message = this._book.GetTitle()});
+        }
+    }
+}
---
+++ Service/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DanskeIT.DotNetCore.Demo2.Service
+{
+    public class Book
+    {
+        private readonly string _title = "Programming C#";
+        public Book()
+        {
+            Console.WriteLine("Creating book...");
+        }
+        public string GetTitle() => this._title;
+    }
+}
